1be8b87edd9efaba27a1c90fe9d8a87e1c93121f
rexsimiloluwah <adetoyosi2001@gmail.com>
2023-08-18 09:31:14 +0100

    fixing amplify deployment errors

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
+  console.log(process.env.PINECONE_INDEX_NAME);
   return NextResponse.json({ status: true, message: "Hello World" });
 }
